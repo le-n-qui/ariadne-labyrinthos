67290ddfecca10d93c7c04a5c6a8b67654f54bc0
Andy Le <andy.quile@gmail.com>
2021-11-02 12:02:30 -0700

    Add comments

--- src/irawanle/cs146/project3/Vertex.java
+++ src/irawanle/cs146/project3/Vertex.java
@@ -4,35 +4,59 @@ public class Vertex {
 	private boolean [] direction = {false, false, false, false};
 	private String color;
 	private Vertex parent;
-//	private int [] coordinates = new int[2];
+    //private int [] coordinates = new int[2];
 	
+	/*
+	 * Constructor
+	 */
 	public Vertex() {
 		parent = null;
 		color = "white";
 	}
 	
-//	0 - North
-//	1 - East
-//	2 - South
-//	3 - West
+	/*
+	 * setDirection method
+	 * 0 - North
+	 * 1 - East
+	 * 2 - South
+	 * 3 - West
+	 * @param index direction
+	 */
 	public void setDirection(int index){
 		direction[index] = true;
 	}
 	
-//	white - unexplored
-//	grey - discovered
-//	black - explored
+	/*
+	 * setColor method
+	 * white - unexplored
+	 * grey - discovered
+     * black - explored
+     * @param c color of vertex
+	 */
 	public void setColor(String c){
 		color = c;
 	}
+	
+	/*
+	 * getColor method
+	 * @return color 
+	 */
 	public String getColor() {
 		return color;
 	}
 	
-//	Link to parent
+	/*
+	 * setParent method
+	 * @param p parent vertex
+	 */
 	public void setParent(Vertex p) {
 		parent = p;
 	}
+	
+	/*
+	 * getParent method
+	 * @return parent vertex
+	 */
 	public Vertex getParent() {
 		return parent;
 	}
