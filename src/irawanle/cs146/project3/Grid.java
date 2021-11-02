263fbfbc2a706822277dfa604d850987fae6524b
Andy Le <andy.quile@gmail.com>
2021-11-02 13:56:16 -0700

    Fix constructor and populateGrid method and add comments

--- src/irawanle/cs146/project3/Grid.java
+++ src/irawanle/cs146/project3/Grid.java
@@ -2,18 +2,23 @@ package irawanle.cs146.project3;
 
 public class Grid {
 	private Vertex [][] mazeGrid;
-	private int rowSize;
-	private int colSize;
+	private int size;
 	
-	public Grid(int row, int col) {
-		mazeGrid = new Vertex[row][col];
-		rowSize = row;
-		colSize = col;
+	/**
+	 * Create a grid
+	 * @param gridSize
+	 */
+	public Grid(int gridSize) {
+		mazeGrid = new Vertex[gridSize][gridSize];
+		size = gridSize;
 	}
 	
+	/**
+	 * Fill grid with cells (vertices)
+	 */
 	private void populateGrid() {
-		for (int i = 0; i < rowSize; i++) {
-			for (int j = 0; j < colSize; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				mazeGrid[i][j] = new Vertex();
 			}
 		}
