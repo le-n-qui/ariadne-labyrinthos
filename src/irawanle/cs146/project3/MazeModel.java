806bf3c44dce96c200da6f1de8d4b5e90caa9275
Andy Le <andy.quile@gmail.com>
2021-11-04 16:13:46 -0700

    Determine available neighbors of current cell

--- src/irawanle/cs146/project3/MazeModel.java
+++ src/irawanle/cs146/project3/MazeModel.java
@@ -1,6 +1,7 @@
 package irawanle.cs146.project3;
 
 import java.util.Random;
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class MazeModel {
@@ -32,7 +33,7 @@ public class MazeModel {
 	 */
 	private void buildMaze() {
 		// Create stack to keep track of cells as they are visited
-		Deque<Vertex> cellStack = new Deque<Vertex>();
+		Deque<Vertex> cellStack = new ArrayDeque<Vertex>();
 		// Total number of cells in maze
 		int totalCells = theMaze.numOfCells();
 		// Cell at (START_COORD_X, START_COORD_Y) will be the starting point
@@ -41,11 +42,25 @@ public class MazeModel {
 		int numVisitedCells = 1;
 		// Create a Random generator
 		Random randSelect = new Random();
-		// Set seed to produce the same result everytime
+		// Set seed to produce the same result every time
 		randSelect.setSeed(totalCells);
 		// A while loop
 		while (numVisitedCells < totalCells) {
 			// find all neighbors of currentCell with all walls intact
+			int currLocX = currentCell.getCoordX();
+			int currLocY = currentCell.getCoordY();
+			if (currLocX - 1 >= 0) {
+				// current cell has a neighbor to the North
+			}
+			if (currLocX + 1 < theMaze.getLimitOfGrid()) {
+				// current cell has a neighbor to the South
+			}
+			if (currLocY + 1 < theMaze.getLimitOfGrid()) {
+				// current cell has a neighbor to the East
+			}
+			if (currLocY - 1 >= 0) {
+				// current cell has a neighbor to the West
+			}
 			// if one or more neighbors found, choose one at random
 				// remove wall between this selected neighbor and currentCell
 				// push currentCell into cellStack
