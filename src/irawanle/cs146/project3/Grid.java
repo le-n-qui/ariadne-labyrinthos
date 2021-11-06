3dc1f9744cce657237441a9b62cccb4daab1fefa
Andy Le <andy.quile@gmail.com>
2021-11-06 00:03:10 -0700

    Provide useful documentation

--- src/irawanle/cs146/project3/Grid.java
+++ src/irawanle/cs146/project3/Grid.java
@@ -1,32 +1,35 @@
 package irawanle.cs146.project3;
 
 public class Grid {
+	// 2-D representation of a grid containing vertices
 	private Vertex [][] mazeGrid;
+	// grid size n  in an n x n grid
 	private int size;
 	
 	/**
-	 * Create a grid
+	 * Constructor: creating a grid given an input size
 	 * @param gridSize
 	 */
 	public Grid(int gridSize) {
 		mazeGrid = new Vertex[gridSize][gridSize];
 		size = gridSize;
-		populateGrid();
+		populateGrid(); // invoke method to fill grid with vertices
 	}
 	
 	/**
-	 * Fill grid with cells (vertices)
+	 * populateGrid method
+	 * fills grid with cells (or vertices)
 	 */
 	private void populateGrid() {
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				mazeGrid[i][j] = new Vertex();
+				mazeGrid[i][j] = new Vertex(i, j);
 			}
 		}
 	}
 	
 	/**
-	 * numOfCells method
+	 * numOfCells method retrieves the total number of cells (vertices)
 	 * @return total number of cells
 	 */
 	public int numOfCells() {
@@ -34,7 +37,7 @@ public class Grid {
 	}
 	
 	/**
-	 * getLimitOfGrid method
+	 * getLimitOfGrid method retrieves the size of grid
 	 * @return the size of the n x n  grid
 	 */
 	public int getLimitOfGrid() {
@@ -42,7 +45,7 @@ public class Grid {
 	}
 	
 	/**
-	 * getCell method
+	 * getCell method retrieves a reference to a cell (vertex) at coordinate (x_loc, y_loc)
 	 * @return reference to the cell
 	 */
 	public Vertex getCell(int x_loc, int y_loc) {
