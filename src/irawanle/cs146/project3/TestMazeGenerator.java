2bab3f42eed0bed8382d463fb4d44742c14dd617
Andy Le <andy.quile@gmail.com>
2021-11-12 16:44:32 -0800

    In setup before class, create a procedure to produce mazes of different sizes with expected solution paths and write them to text files

--- src/irawanle/cs146/project3/TestMazeGenerator.java
+++ src/irawanle/cs146/project3/TestMazeGenerator.java
@@ -1,33 +1,66 @@
 package irawanle.cs146.project3;
 
-/*
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-*/
+
+/**
+ * This class contains unit tests using
+ * JUnit Testing Framework.
+ * @author Andy Qui Le & Ashley Irawan
+ */
 class TestMazeGenerator {
-	MazeReader read;
-	//@BeforeAll
+	static MazeReader read;
+	
+	/**
+	 * This method will prepare objects.
+	 * @throws Exception
+	 */
+	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		// Create a maze reader object
+		read = new MazeReader();
+		
+		// Maze sizes to be tested
+		int[] sizeList = {6, 7};
+		// Create each text file with given size in list
+		for (int size : sizeList) {
+			// Create grid
+			Grid grid = new Grid(size);
+			// Create maze
+			MazeModel maze = new MazeModel(grid);
+			// Build maze
+			maze.buildMaze();
+			// Find solution
+			maze.findEscapeRouteDeeply();
+			// Create a maze view
+			DisplayMaze view = new DisplayMaze(grid);
+			// Mark solution path on maze
+			view.showPath(maze.getDFSSolution());
+			// create text file containing the expected result
+			view.createFile(view.toString(), ("Ateam_maze"+size));
+		}
 	}
 
-	//@BeforeEach
+	
+	@BeforeEach
 	void setUp() throws Exception {
-//		 read = new MazeReader();
-//		 String [][] m = read.readFile("maze4.txt");
-//
-//		 Grid output = read.createMaze(m);
-//		 DisplayMaze display = new DisplayMaze(output);
-//		 System.out.print(display);
-//		 MazeModel solveMaze = new MazeModel(output);
-//		 solveMaze.showResults();
-	}
 
-	//@Test
-	void test() {
-		//fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	void test1() {
+		
+		
+	}
+	
+	@Test 
+	void test2() {
+		
 	}
 
 }
