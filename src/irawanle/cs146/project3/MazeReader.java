a7ee719e88b6db1dfc17b2cd5d72638a369861d6
Andy Le <andy.quile@gmail.com>
2021-11-12 16:45:14 -0800

    Remove an unnecessary library

--- src/irawanle/cs146/project3/MazeReader.java
+++ src/irawanle/cs146/project3/MazeReader.java
@@ -3,8 +3,6 @@ package irawanle.cs146.project3;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-
 
 /**
  * The MazeReader class handles reading 
